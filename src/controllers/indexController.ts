5eeba53a93eb0f2ac9c5e95092a761581e531aab
NBSeven <410805964@qq.com>
2025-03-12 14:18:12 +0800

    feat: 长推修改

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -16,8 +16,8 @@ export const l = async (ctx: Context) => {
 };
 
 export const ln = async (ctx: Context) => {
-    const { title, summary }: any = ctx.request.body;
-    const res = await handleAddStepLN(title, summary);
+    const { title }: any = ctx.request.body;
+    const res = await handleAddStepLN(title);
     ctx.body = { message: res };
 };
 
